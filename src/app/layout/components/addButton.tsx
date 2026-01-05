8011b0a67e17363e4eda60b07ecb94f03df0a68e
Marcelo Delgado <marcelo.delgadomora@ucr.ac.cr>
2026-01-05 17:56:19 -0600

    Color fixing for general styles after shadcn implementation

--- src/app/layout/components/addButton.tsx
+++ src/app/layout/components/addButton.tsx
@@ -16,25 +16,25 @@ export function AddButton() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="flex items-center mr-6 px-4 py-2 bg-primary text-white rounded hover:bg-primary-hover transition-colors hover:cursor-pointer">
+        <button className="flex items-center mr-6 px-4 py-2 bg-primary text-white rounded hover:bg-primary-hover transition-colors hover:cursor-pointer outline-none focus:outline-none focus-visible:ouline-none">
           <Plus className="inline mr-2" />
           Add
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-surface border border-border rounded shadow-md mt-2">
-        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer">
+      <DropdownMenuContent className="bg-surface border border-border rounded shadow-md mt-4">
+        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer outline-none focus:outline-none focus-visible:ouline-none">
           <BanknoteArrowUp className="inline mr-2 size-4 stroke-[1.5] text-text-muted" />
           Income
         </DropdownMenuItem>
-        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer">
+        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer outline-none focus:outline-none focus-visible:ouline-none">
           <CreditCard className="inline mr-2 size-4 stroke-[1.5] text-text-muted" />
           Transaction
         </DropdownMenuItem>
-        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer">
+        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer outline-none focus:outline-none focus-visible:ouline-none">
           <CircleDollarSign className="inline mr-2 size-4 stroke-[1.5] text-text-muted" />
           Account
         </DropdownMenuItem>
-        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer">
+        <DropdownMenuItem className="px-4 py-2 hover:bg-surface-hover hover:cursor-pointer outline-none focus:outline-none focus-visible:ouline-none">
           <Folder className="inline mr-2 size-4 stroke-[1.5] text-text-muted" />
           Category
         </DropdownMenuItem>
--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -4,12 +4,12 @@ import { AddButton } from "./components/addButton";
 export function Header() {
   return (
     <header className="h-16 flex justify-between items-center px-6 bg-surface border-b border-border sticky top-0 z-40 backdrop-blur bg-surface/95">
-      <h1 className="font-semibold text-lg text-text">Ledger</h1>
+      <h1 className="font-semibold text-lg text-foreground">Ledger</h1>
       <div className="flex">
         <AddButton />
         <div className="flex items-center bg-surface-hover rounded-full">
-          <Bell className="size-6 stroke-[1.5] text-text-muted hover:text-primary hover:cursor-pointer mr-6 transition-colors" />
-          <User className="size-6 stroke-[1.5] text-text-muted hover:text-primary hover:cursor-pointer transition-colors" />
+          <Bell className="size-6 stroke-[1.5] text-muted-foreground hover:text-primary hover:cursor-pointer mr-6 transition-colors" />
+          <User className="size-6 stroke-[1.5] text-muted-foreground hover:text-primary hover:cursor-pointer transition-colors" />
         </div>
       </div>
     </header>
