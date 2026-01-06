f1701d97d45b8fa8dbcb3d484bca83e9c022e3cc
Marcelo Delgado <marcelo.delgadomora@ucr.ac.cr>
2026-01-05 18:01:47 -0600

    Color text change

--- src/app/layout/sidebar.tsx
+++ src/app/layout/sidebar.tsx
@@ -11,24 +11,24 @@ export function Sidebar() {
     <aside className="w-54 bg-surface border-r border-border p-4">
       <nav>
         <ul>
-          <li className="flex items-center mb-4 hover:text-primary cursor-pointer transition-colors transform transition hover:scale-105">
-            <LayoutDashboard className=" mr-2" />
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+            <LayoutDashboard className="mr-2 text-foreground" />
             Dashboard
           </li>
-          <li className="flex items-center mb-4 hover:text-primary cursor-pointer transition-colors transform transition hover:scale-105">
-            <CreditCard className=" mr-2" />
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+            <CreditCard className="mr-2 text-foreground" />
             Transactions
           </li>
-          <li className="flex items-center mb-4 hover:text-primary cursor-pointer transition-colors transform transition hover:scale-105">
-            <Folder className=" mr-2" />
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+            <Folder className="mr-2 text-foreground" />
             Category
           </li>
-          <li className="flex items-center mb-4 hover:text-primary cursor-pointer transition-colors transform transition hover:scale-105">
-            <PiggyBank className="mr-2" />
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+            <PiggyBank className="mr-2 text-foreground" />
             Budgets
           </li>
-          <li className="flex items-center mb-4 hover:text-primary cursor-pointer transition-colors transform transition hover:scale-105">
-            <ClipboardPen className="mr-2" />
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+            <ClipboardPen className="mr-2 text-foreground" />
             Reports
           </li>
         </ul>
