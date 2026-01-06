1a3efd0ede913329fb314ca0eb2e1cafe5f7375f
Marcelo Delgado <marcelo.delgadomora@ucr.ac.cr>
2026-01-06 10:10:30 -0600

    sidebar visual changes

--- src/app/layout/sidebar.tsx
+++ src/app/layout/sidebar.tsx
@@ -4,35 +4,42 @@ import {
   Folder,
   LayoutDashboard,
   PiggyBank,
+  Settings,
 } from "lucide-react";
 
 export function Sidebar() {
   return (
-    <aside className="w-54 bg-surface border-r border-border p-4">
+    <aside className="w-45 bg-surface border-r border-border p-4">
       <nav>
         <ul>
-          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-115 hover:translate-x-5">
             <LayoutDashboard className="mr-2 text-foreground" />
             Dashboard
           </li>
-          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-115 hover:translate-x-5">
             <CreditCard className="mr-2 text-foreground" />
             Transactions
           </li>
-          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-115 hover:translate-x-5">
             <Folder className="mr-2 text-foreground" />
             Category
           </li>
-          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-115 hover:translate-x-5">
             <PiggyBank className="mr-2 text-foreground" />
             Budgets
           </li>
-          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-105">
+          <li className="flex items-center mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-115 hover:translate-x-5">
             <ClipboardPen className="mr-2 text-foreground" />
             Reports
           </li>
         </ul>
       </nav>
+      <div className="absolute bottom-4 w-45">
+        <li className="flex items-center self-end mb-4 hover:text-foreground cursor-pointer transition-colors transform transition hover:scale-115 hover:translate-x-5">
+          <Settings className="mr-2 text-foreground" />
+          Settings
+        </li>
+      </div>
     </aside>
   );
 }
